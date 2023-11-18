3c8fe019f15194f0ca9d244f8e6994290e495c4b
Moshe Brizel <brizelmoshe@gmail.com>
2023-11-18 21:21:52 +0200

    add the login

---
+++ controllers/routeAuthfunctions/logIn.js
@@ -0,0 +1,54 @@
+import { getOneUser } from "../../db/controller/functionsDBUser";
+import bcrypt from "bcrypt";
+import JWT from "jsonwebtoken";
+
+const loginFunction = async (req, res) => {
+  // get the user name and the password.
+  const { userName, password } = req.body.sendData;
+  // check if the user is in the DB.
+  let user = await getOneUser({ userName: userName });
+  // if it's empty it's send a erorr.
+  if (!userName) {
+    return res.status(400).json({
+      errors: {
+        msg: "Invalid Credentials",
+      },
+    });
+  }
+  // check if the password is corecct.
+  let corectPassword = await bcrypt.compare(password, user.password);
+  // if it's not a corect password it's send a eroor.
+  if (!corectPassword) {
+    return res.status(400).json({
+      errors: {
+        msg: "Invalid Credentials",
+      },
+    });
+  }
+
+  const sendInformtionUser = {
+    id: user._id,
+    billsVote: user.billsVote,
+    gender: user.gender,
+    identity: user.identity,
+    party: user.party,
+    userName: user.userName,
+  };
+  // creat a token with the email inside.
+  const token = JWT.sign(
+    {
+      userName,
+    },
+    process.env.SICRET_KEY_TOKEN,
+    {
+      expiresIn: 3600000,
+    }
+  );
+  // send the token.
+  res.status(200).json({
+    token: token,
+    user: sendInformtionUser,
+  });
+};
+
+export default loginFunction;
--- routes/routerAuth.js
+++ routes/routerAuth.js
@@ -3,13 +3,14 @@ import express from 'express'
 // functions
 import signupFunction from '../controllers/routeAuthfunctions/signup'
 import verifyEmailFunction from '../controllers/routeAuthfunctions/verifyEmail'
+import loginFunction from '../controllers/routeAuthfunctions/logIn'
 
 const router = express.Router()
 
 
 router.post("/signup",signupFunction)
 router.post("/verifyEmail",verifyEmailFunction)
-
+router.post("/login", loginFunction)
 
 
 
