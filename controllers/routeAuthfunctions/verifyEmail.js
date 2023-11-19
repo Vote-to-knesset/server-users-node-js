1fa436d8067b4cf6d48a7ffd7cb94b81d9b82770
Moshe Brizel <brizelmoshe@gmail.com>
2023-11-19 16:35:54 +0200

    add user name router

--- controllers/routeAuthfunctions/verifyEmail.js
+++ controllers/routeAuthfunctions/verifyEmail.js
@@ -1,58 +1,6 @@
-import { getOneEmail } from "../../db/controller/functionsDBEmails.js";
-import calculateDateDifference from "../../functions/calculateDateDifference.js";
-import { addOneUser } from "../../db/controller/functionsDBUser.js";
 
-const verifyEmailFunction = async (req, res) => {
-  const { emailCode, email, userName, password, party, identity, gender } =
-    req.body;
 
-  if (
-    !emailCode ||
-    !email ||
-    !userName ||
-    !password ||
-    !party ||
-    !identity ||
-    !gender
-  ) {
-    return res.status(401).json({
-      msg: "one is empty",
-    });
-  }
-  const emailUser = await getOneEmail({
-    email: email,
-    "verifyEmail.value": emailCode,
-  });
-  if (!emailUser) {
-    return res.status(401).json({
-      msg: "one of the informtion is wong",
-    });
-  }
-  const time = calculateDateDifference(verifyEmail.date, new Date());
-  if (time.hours > 0 || time.minutes > 10) {
-    return res.status(401).json({
-      msg: "time is over",
-    });
-  }
-  let hashePassword = await bcrypt.hash(password, 10);
-  const addUser = await addOneUser({
-    userName: userName,
-    password: hashePassword,
-    date: new Date(),
-    party: party,
-    identity: identity,
-    gender: gender,
-  });
+const verifyEmailFunction = async (req,res) => {
 
-  if (addUser == true) {
-    return res.status(200).json({
-      msg: "the user is added",
-    });
-  } else {
-    return res.status(401).json({
-      msg: "erorr",
-    });
-  }
-};
-
-export default verifyEmailFunction;
+}
+export default verifyEmailFunction
