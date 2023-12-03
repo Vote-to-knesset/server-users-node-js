d3b3e7b15bc9f7d8168997940dceb69b1a35db57
eli barak <barak3656@gmail.com>
2023-12-03 13:12:30 +0200

    change the logic of code verify

--- controllers/routeAuthfunctions/verifyEmail.js
+++ controllers/routeAuthfunctions/verifyEmail.js
@@ -5,12 +5,23 @@ import {
 
 const verifyEmailFunction = async (req, res) => {
   const { email, code } = req.body;
-  const userEmail = getOneEmail({ email: email, "verifyEmail.value": code });
+  const userEmail = getOneEmail({ email: email});
+
+
   if (!userEmail) {
     return res.status(401).json({
       msg: "one of the informtion is wong",
     });
   }
+
+  
+  if (userEmail.verifyEmail.value !== code){
+    return res.status(401).json({
+      msg: "one of the informtion is wong",
+    });
+
+  }
+  
   const updateEmail = await updeteOneEmail(email, { "verifyEmail.verify": true });
   if (updateEmail == true) {
     return res.status(200).json({
